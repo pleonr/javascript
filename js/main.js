f2fe589193874a78c7b76c80a974c9e05e53c5ca
pleonr <pablo.leonrodrigues@gmail.com>
2025-05-27 19:25:14 -0300

    Commit inicial do projeto

---
+++ js/main.js
@@ -0,0 +1,60 @@
+let corpo = document.body
+
+function themeBlack() {
+  corpo.className = ""
+  corpo.classList.add("dark")
+}
+
+function themeGreen() {
+  corpo.className = ""
+  corpo.classList.add("green")
+}
+
+function themeGrey() {
+  corpo.className = ""
+  corpo.classList.add("grey")
+}
+
+function salvar() {
+  let nome = document.getElementById("nome").value
+  let curso = document.getElementById("curso").value
+  let universidade = document.getElementById("universidade").value
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
